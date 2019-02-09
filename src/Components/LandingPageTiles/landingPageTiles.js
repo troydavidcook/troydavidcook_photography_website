62428047d08c2d1239eb7d62fedc0156502d0372
Troy David Cook <troydavidcook@troys-mbp.home>
2019-02-08 19:30:23 -0500

    breakpoints actaully where I want them.

--- src/Components/LandingPageTiles/landingPageTiles.js
+++ src/Components/LandingPageTiles/landingPageTiles.js
@@ -6,26 +6,26 @@ function ImageTiles (props) {
     return (
         <Container>
             <Row className="Splash-Images">
-                <Col className="col-4" col-sm-2 col-xs-1>
-                    <img className="imageTile" p-1 flex-fill src={props.tiles[0]} alt=""/>
+                <Col xs={12} sm={6} md={4} lg={4} >
+                    <img className="imageTile" src={props.tiles[0]} alt=""/>
                 </Col>
-                <Col className="col-4" col-sm-2 col-xs-1>
-                <img className="imageTile" p-1 flex-fill src={props.tiles[1]} alt=""/>
+                <Col xs={12} sm={6} md={4} lg={4} >
+                <img className="imageTile" src={props.tiles[1]} alt=""/>
                 </Col>
-                <Col className="col-4" col-sm-2 col-xs-1>
-                <img className="imageTile" p-1 flex-fill src={props.tiles[2]} alt=""/>
+                <Col xs={12} sm={6} md={4} lg={4} >
+                <img className="imageTile" src={props.tiles[2]} alt=""/>
                 </Col>
-                <Col className="col-4" col-sm-2 col-xs-1>
-                <img className="imageTile" p-1 flex-fill src={props.tiles[3]} alt=""/>
+                <Col xs={12} sm={6} md={4} lg={4} >
+                <img className="imageTile" src={props.tiles[3]} alt=""/>
                 </Col>
-                <Col className="col-4" col-sm-2 col-xs-1>
-                <img className="imageTile" p-1 flex-fill src={props.tiles[4]} alt=""/>
+                <Col xs={12} sm={6} md={4} lg={4} >
+                <img className="imageTile" src={props.tiles[4]} alt=""/>
                 </Col>
-                <Col className="col-4" col-sm-2 col-xs-1>
-                <img className="imageTile" p-1 flex-fill src={props.tiles[5]} alt=""/>
+                <Col xs={12} sm={6} md={4} lg={4} >
+                <img className="imageTile" src={props.tiles[5]} alt=""/>
                 </Col>
-                <Col className="col-4" col-sm-2 col-xs-1>
-                <img className="imageTile" p-1 flex-fill src={props.tiles[6]} alt=""/>
+                <Col xs={12} sm={6} md={4} lg={4} >
+                <img className="imageTile" src={props.tiles[6]} alt=""/>
                 </Col>
             </Row>
         </Container>
